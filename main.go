80254a3eabc8a3f6b9fab0f25fa863e1b2b4ae4d
voloshmiak <voloshenkomykhailo@gmail.com>
2025-04-11 20:20:46 +0300

    graceful shutdown added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"forum-project/internal/handlers"
@@ -9,8 +10,28 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
+
+func gracefulShutdown(server *http.Server, logger *slog.Logger, done chan bool) {
+	// wait for interruption
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	<-ctx.Done()
+
+	logger.Info("Shutting down server gracefully")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		logger.Error(fmt.Sprintf("Server forced to shutdown: %s", err))
+	}
+
+	done <- true
+}
 
 func main() {
 	// initialize logger
@@ -46,7 +67,7 @@ func main() {
 	mux.HandleFunc("GET /posts/{id}", ph.GetPost)
 
 	// server configuration
-	server := http.Server{
+	server := &http.Server{
 		Addr:         ":" + os.Getenv("PORT"),
 		Handler:      mux,
 		ReadTimeout:  5 * time.Second,
@@ -54,10 +75,19 @@ func main() {
 		IdleTimeout:  15 * time.Second,
 	}
 
+	done := make(chan bool, 1)
+
+	go gracefulShutdown(server, logger, done)
+
 	// start serving server
 	logger.Info(fmt.Sprintf("Starting server on port %s", server.Addr))
 
 	if err = server.ListenAndServe(); err != nil && !errors.Is(http.ErrServerClosed, err) {
-		logger.Error("Error listening and serving", err)
+		logger.Error(fmt.Sprintf("Error listening and serving: %s", err))
+		os.Exit(1)
 	}
+
+	// wait for graceful shutdown to complete
+	<-done
+	logger.Info("Server shutdown complete")
 }
