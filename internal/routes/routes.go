3b8fef92634e544580ffecc0680ff404b2aa92e1
voloshmiak <voloshenkomykhailo@gmail.com>
2025-05-23 22:02:21 +0300

    refactored routes init, group into one func

---
+++ internal/routes/routes.go
@@ -0,0 +1,65 @@
+package routes
+
+import (
+	"forum-project/internal/application"
+	"forum-project/internal/handler"
+	"forum-project/internal/middleware"
+	"net/http"
+)
+
+func Register(app *application.App) *http.ServeMux {
+	// Handlers
+	hh := handler.NewHomeHandler(app)
+	ph := handler.NewPostHandler(app)
+	th := handler.NewTopicHandler(app)
+	uh := handler.NewUserHandler(app)
+
+	// Mux
+	mux := http.NewServeMux()
+	authorizedMux := http.NewServeMux()
+	adminMux := http.NewServeMux()
+
+	// Middleware
+	auth := middleware.AuthMiddleware
+	isPostAuthor := middleware.IsPostAuthor(app)
+	isAdmin := middleware.CreateStack(middleware.AuthMiddleware, middleware.IsAdmin)
+	isPostAuthorOrAdmin := middleware.IsPostAuthorOrAdmin(app)
+
+	// Static
+	fileserver := http.FileServer(http.Dir("./web/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+
+	// Home
+	mux.HandleFunc("GET /home", hh.GetHome)
+	mux.HandleFunc("GET /about", hh.GetAbout)
+
+	// User
+	mux.HandleFunc("GET /login", uh.GetLogin)
+	mux.HandleFunc("POST /login", uh.PostLogin)
+	mux.HandleFunc("GET /logout", uh.GetLogout)
+	mux.HandleFunc("GET /signup", uh.GetRegister)
+	mux.HandleFunc("POST /signup", uh.PostRegister)
+
+	// Post
+	mux.HandleFunc("GET /topics/{topicID}/posts/{postID}", ph.GetPost)
+	authorizedMux.HandleFunc("GET /topics/{topicID}/posts/new", ph.GetCreatePost)
+	authorizedMux.HandleFunc("POST /posts", ph.PostCreatePost)
+	authorizedMux.Handle("GET /posts/{postID}/edit", isPostAuthor(http.HandlerFunc(ph.GetEditPost)))
+	authorizedMux.Handle("POST /posts/{postID}/edit", isPostAuthor(http.HandlerFunc(ph.PostEditPost)))
+	authorizedMux.Handle("GET /posts/{postID}/delete", isPostAuthorOrAdmin(http.HandlerFunc(ph.GetDeletePost)))
+
+	mux.Handle("/user/", http.StripPrefix("/user", auth(authorizedMux))) // grouping
+
+	// Topic
+	mux.HandleFunc("GET /topics", th.GetTopics)
+	mux.HandleFunc("GET /topics/{topicID}", th.GetTopic)
+	adminMux.HandleFunc("GET /topics/new", th.GetCreateTopic)
+	adminMux.HandleFunc("POST /topics", th.PostCreateTopic)
+	adminMux.HandleFunc("GET /topics/{topicID}/edit", th.GetEditTopic)
+	adminMux.HandleFunc("POST /topics/{topicID}/edit", th.PostEditTopic)
+	adminMux.HandleFunc("GET /topics/{topicID}/delete", th.GetDeleteTopic)
+
+	mux.Handle("/admin/", http.StripPrefix("/admin", isAdmin(adminMux))) // grouping
+
+	return mux
+}
