0c9226536f7bc6149f4a0071dd64eca784f19f7f
voloshmiak <voloshenkomykhailo@gmail.com>
2025-05-25 18:10:29 +0300

    removed statuc code from structure logging

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -3,34 +3,16 @@ package middleware
 import (
 	"log/slog"
 	"net/http"
-	"strconv"
 )
-
-type wrappedWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (w *wrappedWriter) WriteHeader(statusCode int) {
-	w.statusCode = statusCode
-	w.ResponseWriter.WriteHeader(statusCode)
-}
 
 func Logging(next http.Handler, logger *slog.Logger) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-
-		ww := &wrappedWriter{
-			ResponseWriter: rw,
-			statusCode:     http.StatusOK,
-		}
-
-		next.ServeHTTP(rw, r)
-
 		logger.Info(
 			"HTTP request",
 			"method", r.Method,
-			"status", strconv.Itoa(ww.statusCode),
 			"path", r.URL.Path,
 		)
+
+		next.ServeHTTP(rw, r)
 	})
 }
