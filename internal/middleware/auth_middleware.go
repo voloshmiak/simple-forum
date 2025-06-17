f0715588c3d2b4c6ab45cb2f9ccc9369632c18ee
voloshmiak <voloshenkomykhailo@gmail.com>
2025-06-17 11:48:05 +0300

    refactor auth middleware to use JwtAuthenticator for claims extraction

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -2,29 +2,17 @@ package middleware
 
 import (
 	"context"
-	"errors"
-	"forum-project/internal/app"
+	"forum-project/internal/auth"
 	"forum-project/internal/model"
-	"forum-project/internal/service"
 	"net/http"
 )
 
-func AuthMiddleware(app *app.App) func(http.Handler) http.Handler {
+func AuthMiddleware(auther *auth.JwtAuthenticator) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-			cookie, err := r.Cookie("token")
+			claims, err := auther.GetClaimsFromRequest(r)
 			if err != nil {
 				http.Error(rw, "Unauthorized", http.StatusUnauthorized)
-				return
-			}
-			claims, err := service.ValidateToken(cookie.Value, app.Config.JWT.Secret)
-			if err != nil {
-				if errors.Is(err, service.ErrInvalidToken) {
-					http.Error(rw, "Invalid token", http.StatusUnauthorized)
-					return
-				}
-				http.Error(rw, "Invalid token", http.StatusInternalServerError)
-				app.Logger.Error("Failed to validate token", "error", err)
 				return
 			}
 
