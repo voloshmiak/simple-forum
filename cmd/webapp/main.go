134d629f80c8bfa580d8e190b1349bcbe29fea9d
Mykhailo <voloshenkomykhailo@gmail.com>
2025-07-08 17:23:58 +0300

    removed `app` package references from `main.go`; refactored to use dedicated instances for services, middleware, handlers, and logger; updated routing and server initialization logic

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -4,15 +4,23 @@ import (
 	"context"
 	"errors"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
-	"simple-forum/internal/app"
+	"path/filepath"
+	"simple-forum/internal/auth"
 	"simple-forum/internal/config"
-	"simple-forum/internal/db"
-	"simple-forum/internal/router"
+	"simple-forum/internal/database"
+	"simple-forum/internal/handler"
+	"simple-forum/internal/middleware"
+	"simple-forum/internal/repository"
+	"simple-forum/internal/service"
+	"simple-forum/internal/template"
 	"syscall"
 	"time"
+
+	"github.com/justinas/nosurf"
 )
 
 func main() {
@@ -29,36 +37,102 @@ func run() error {
 	}
 
 	// Migrate
-	err = db.Migrate(cfg.DB.Addr, cfg.Path.ToMigrations)
+	err = database.Migrate(cfg.DB.Addr, cfg.Path.ToMigrations)
 	if err != nil {
 		return err
 	}
 
 	// Connection
-	conn, err := db.NewConnection(cfg.DB.Addr)
+	conn, err := database.Connect(cfg.DB.Addr)
 	if err != nil {
 		return err
 	}
 	defer conn.Close()
 
-	// App
-	a := app.New(conn, cfg)
+	// Logger
+	l := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-	// Router
-	r := router.RegisterRoutes(a)
+	// Authenticator
+	a := auth.NewJWTAuthenticator(cfg.JWT.Secret, cfg.JWT.Expiration)
+
+	// Templates
+	t := template.NewTemplates(cfg.Env, cfg.Path.ToTemplates, a)
+
+	// Repository
+	postRepository := repository.NewPostRepository(conn)
+	topicRepository := repository.NewTopicRepository(conn)
+	userRepository := repository.NewUserRepository(conn)
+
+	// Service
+	postService := service.NewPostService(postRepository)
+	topicService := service.NewTopicService(topicRepository)
+	userService := service.NewUserService(userRepository)
+
+	// Handlers
+	hh := handler.NewHomeHandler(l, t)
+	ph := handler.NewPostHandler(l, a, t, postService, topicService)
+	th := handler.NewTopicHandler(l, a, t, postService, topicService)
+	uh := handler.NewUserHandler(l, a, t, userService)
+
+	// Mux
+	mux := http.NewServeMux()
+	authMux := http.NewServeMux()
+	adminMux := http.NewServeMux()
+
+	// Middleware
+	adminMiddleware := middleware.PermissionMiddleware(l, postService, "admin")
+	authorMiddleware := middleware.PermissionMiddleware(l, postService, "author")
+	sharedMiddleware := middleware.PermissionMiddleware(l, postService, "admin", "author")
+	authMiddleware := middleware.AuthMiddleware(a)
+	loggingMiddleware := middleware.LoggingMiddleware(l)
+
+	// ToStatic
+	fileserver := http.FileServer(http.Dir(filepath.ToSlash(cfg.Path.ToStatic)))
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+
+	// Home
+	mux.HandleFunc("GET /home", hh.GetHome)
+	mux.HandleFunc("GET /about", hh.GetAbout)
+
+	// User
+	mux.HandleFunc("GET /login", uh.GetLogin)
+	mux.HandleFunc("POST /login", uh.PostLogin)
+	mux.HandleFunc("GET /logout", uh.GetLogout)
+	mux.HandleFunc("GET /signup", uh.GetRegister)
+	mux.HandleFunc("POST /signup", uh.PostRegister)
+
+	// Post
+	mux.HandleFunc("GET /topics/{topicID}/posts/{postID}", ph.GetPost)
+	authMux.HandleFunc("GET /topics/{topicID}/posts/new", ph.GetCreatePost)
+	authMux.HandleFunc("POST /posts", ph.PostCreatePost)
+	authMux.HandleFunc("GET /posts/{postID}/edit", authorMiddleware(http.HandlerFunc(ph.GetEditPost)))
+	authMux.HandleFunc("POST /posts/{postID}/edit", authorMiddleware(http.HandlerFunc(ph.PostEditPost)))
+	authMux.HandleFunc("GET /posts/{postID}/delete", sharedMiddleware(http.HandlerFunc(ph.GetDeletePost)))
+
+	mux.Handle("/user/", http.StripPrefix("/user", authMiddleware(authMux))) // grouping
+
+	// Topic
+	mux.HandleFunc("GET /topics", th.GetTopics)
+	mux.HandleFunc("GET /topics/{topicID}", th.GetTopic)
+	adminMux.HandleFunc("GET /topics/new", th.GetCreateTopic)
+	adminMux.HandleFunc("POST /topics", th.PostCreateTopic)
+	adminMux.HandleFunc("GET /topics/{topicID}/edit", th.GetEditTopic)
+	adminMux.HandleFunc("POST /topics/{topicID}/edit", th.PostEditTopic)
+	adminMux.HandleFunc("GET /topics/{topicID}/delete", th.GetDeleteTopic)
+
+	mux.Handle("/admin/", http.StripPrefix("/admin", authMiddleware(adminMiddleware(adminMux)))) // grouping
 
 	// Server
 	server := &http.Server{
 		Addr:         ":" + cfg.Server.Port,
-		Handler:      r,
+		Handler:      nosurf.New(loggingMiddleware(mux)),
 		ReadTimeout:  time.Duration(cfg.Server.ReadTimeout) * time.Second,
 		WriteTimeout: time.Duration(cfg.Server.WriteTimeout) * time.Second,
 		IdleTimeout:  time.Duration(cfg.Server.IdleTimeout) * time.Second,
 	}
 
-	// Serve
-
-	a.Logger.Info("Starting server on port: " + server.Addr)
+	// Listening
+	l.Info("Starting server on port: " + server.Addr)
 
 	done := make(chan bool)
 
@@ -67,13 +141,13 @@ func run() error {
 		signal.Notify(signs, syscall.SIGINT, syscall.SIGTERM)
 
 		<-signs
-		a.Logger.Info("Shutting down server gracefully")
+		l.Info("Shutting down server gracefully")
 
 		shutdownCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 		defer cancel()
 
 		if err = server.Shutdown(shutdownCtx); err != nil {
-			a.Logger.Error("Server forced to shutdown", "error", err)
+			l.Error("Server forced to shutdown", "error", err)
 		}
 
 		done <- true
@@ -84,7 +158,7 @@ func run() error {
 	}
 
 	<-done
-	a.Logger.Info("Graceful shutdown complete")
+	l.Info("Graceful shutdown complete")
 
 	return nil
 }
