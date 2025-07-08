fe488b658370acf3621da26425125a222858cc2b
Mykhailo <voloshenkomykhailo@gmail.com>
2025-07-08 17:21:23 +0300

    added constraints and adjusted default values for `created_at`; updated `users` seed insertion format

--- migrations/000001_create_initial_tables.up.sql
+++ migrations/000001_create_initial_tables.up.sql
@@ -3,10 +3,9 @@ CREATE TABLE topics
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(255) NOT NULL,
     description TEXT         NOT NULL,
-    created_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    author_id   int          NOT NULL
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    author_id   int          NOT NULL REFERENCES users (id)
 );
-
 CREATE TABLE posts
 (
     id         SERIAL PRIMARY KEY,
@@ -15,7 +14,7 @@ CREATE TABLE posts
     author_id  int          NOT NULL,
     author_name VARCHAR(50) NOT NULL,
     topic_id   INTEGER      NOT NULL REFERENCES topics (id) ON DELETE CASCADE,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -29,5 +28,10 @@ CREATE TABLE users
     role VARCHAR(20) NOT NULL CHECK (role IN ('user', 'admin')) DEFAULT 'user'
 );
 
-INSERT INTO users (id, username, email, password_hash, role)
-VALUES (1, 'admin', 'admin@email.com', '$2a$10$sMn.IWt9q3EiisAecQoOLOsvnA0wsl2oMRDGcHIrAR6XNOBVpxILK', 'admin');
+INSERT INTO users (username, email, password_hash, role)
+VALUES (
+  'admin',
+  'admin@email.com',
+  '$2a$10$sMn.IWt9q3EiisAecQoOLOsvnA0wsl2oMRDGcHIrAR6XNOBVpxILK',
+  'admin'
+);
