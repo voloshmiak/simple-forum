e45552cbe0971fe9625bb901fb8d58279a24c481
Mykhailo <voloshenkomykhailo@gmail.com>
2025-07-09 00:56:45 +0300

    renamed `username` tag to `name`

--- internal/model/user.go
+++ internal/model/user.go
@@ -4,7 +4,7 @@ import "time"
 
 type User struct {
 	ID           int       `json:"id"`
-	Name         string    `json:"username"`
+	Name         string    `json:"name"`
 	Email        string    `json:"email"`
 	PasswordHash string    `json:"password_hash"`
 	CreatedAt    time.Time `json:"created_at"`
